20f1a4f03c99e3e217c9722e4678c9c8275e48d9
Chika Dim <cckrist@outlook.com>
2023-12-08 01:18:11 +0000

    Add create, update and delete comment function

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -1,43 +1,112 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
 
 import appStyles from "../../App.module.css";
+import styles from "../../styles/PostPage.module.css";
 import { useParams } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
 import Post from "./Post";
+import Comment from "../comments/Comment";
+import CommentCreateForm from "../comments/CommentCreateForm";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import InfiniteScroll from "react-infinite-scroll-component";
+import Asset from "../../components/Asset";
+import { fetchMoreData } from "../../utils/utils";
+import Tooltip from "@mui/material/Tooltip";
+import { Link } from "react-router-dom";
 
 function PostPage() {
+  const [hasLoaded, setHasLoaded] = useState(false);
   const { id } = useParams();
   const [post, setPost] = useState({ results: [] });
+
+  const currentUser = useCurrentUser();
+  const profile_image = currentUser?.profile_image;
+  const [comments, setComments] = useState({ results: [] });
 
   useEffect(() => {
     const handleMount = async () => {
       try {
-        const [{ data: post }] = await Promise.all([
+        const [{ data: post }, { data: comments }] = await Promise.all([
           axiosReq.get(`/posts/${id}`),
+          axiosReq.get(`/comments/?post=${id}`),
         ]);
         setPost({ results: [post] });
-        console.log(post);
+        setComments(comments);
+        setHasLoaded(true);
       } catch (err) {
-        console.log(err);
+        // console.log(err);
       }
     };
 
-    handleMount();
+    setHasLoaded(false);
+    const timer = setTimeout(() => {
+      handleMount();
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
   }, [id]);
 
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
-        <p>Popular profiles for mobile</p>
-        <Post {...post.results[0]} setPosts={setPost} postPage />
-        <Container className={appStyles.Content}>Comments</Container>
+        
+        {hasLoaded ? (
+          <>
+            <Post {...post.results[0]} setPosts={setPost} postPage />
+            <Container className={`${appStyles.Content} ${styles.Comments}`}>
+              {currentUser ? (
+                <CommentCreateForm
+                  profile_id={currentUser.profile_id}
+                  profileImage={profile_image}
+                  post={id}
+                  setPost={setPost}
+                  setComments={setComments}
+                />
+              ) : comments.results.length ? (
+                <h5>Comments</h5>
+              ) : null}
+              {comments.results.length ? (
+                <InfiniteScroll
+                  // eslint-disable-next-line react/no-children-prop
+                  children={comments.results.map((comment) => (
+                    <Comment
+                      key={comment.id}
+                      {...comment}
+                      setPost={setPost}
+                      setComments={setComments}
+                    />
+                  ))}
+                  dataLength={comments.results.length}
+                  loader={<Asset spinner />}
+                  hasMore={!!comments.next}
+                  next={() => fetchMoreData(comments, setComments)}
+                />
+              ) : currentUser ? (
+                <span className={styles.NoComment}>
+                  No comments yet, be the first to comment!
+                </span>
+              ) : (
+                <Tooltip title="Please login to comment!" placement="top" arrow>
+                  <Link to={"/signin"}>
+                    <span className={styles.NoComment}>No comments... yet</span>
+                  </Link>
+                </Tooltip>
+              )}
+            </Container>
+            
+          </>
+        ) : (
+          <Asset spinner />
+        )}
       </Col>
       <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
-        Popular profiles for desktop
+        
       </Col>
     </Row>
   );
